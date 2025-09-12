3f27c2c9a323ef33e498eef4ccc5b9650a64e8f1
xinlin-z <xinlin.zhang@hotmail.com>
2025-09-12 13:31:46 +1200

    change tables

--- init_iotdb.sql
+++ init_iotdb.sql
@@ -2,7 +2,6 @@
 
 /*DROP TABLE IF EXISTS Switch;
 DROP TABLE IF EXISTS Temperature;
-DROP TABLE IF EXISTS Sound;
 DROP TABLE IF EXISTS Motion1;
 DROP TABLE IF EXISTS Motion2;
 DROP TABLE IF EXISTS Image;*/
@@ -11,6 +10,7 @@ DROP TABLE IF EXISTS Image;*/
 -- Create Switch table
 CREATE TABLE Switch (
     id SERIAL PRIMARY KEY,
+    session INTEGER NOT NULL,
     datetime TIMESTAMP NOT NULL,
     status BOOLEAN NOT NULL
 );
@@ -18,13 +18,7 @@ CREATE TABLE Switch (
 -- Create Temperature table
 CREATE TABLE Temperature (
     id SERIAL PRIMARY KEY,
-    datetime TIMESTAMP NOT NULL,
-    value DECIMAL(6,2) NOT NULL
-);
-
--- Create Sound table
-CREATE TABLE Sound (
-    id SERIAL PRIMARY KEY,
+    session INTEGER NOT NULL,
     datetime TIMESTAMP NOT NULL,
     value DECIMAL(6,2) NOT NULL
 );
@@ -32,20 +26,23 @@ CREATE TABLE Sound (
 -- Create Motion1 table
 CREATE TABLE Motion1 (
     id SERIAL PRIMARY KEY,
+    session INTEGER NOT NULL,
     datetime TIMESTAMP NOT NULL,
-    value DECIMAL(6,2) NOT NULL
+    value BOOLEAN NOT NULL
 );
 
 -- Create Motion2 table
 CREATE TABLE Motion2 (
     id SERIAL PRIMARY KEY,
+    session INTEGER NOT NULL,
     datetime TIMESTAMP NOT NULL,
-    value DECIMAL(6,2) NOT NULL
+    value BOOLEAN NOT NULL
 );
 
 -- Create Image table
 CREATE TABLE Image (
     id SERIAL PRIMARY KEY,
+    session INTEGER NOT NULL,
     datetime TIMESTAMP NOT NULL,
     ingredient VARCHAR(1024) NOT NULL,
     style VARCHAR(32) NOT NULL
